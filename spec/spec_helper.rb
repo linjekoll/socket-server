aeb14d1fe5cfdab011c5f49c241b7837b3a04b1d
Linus Oleander <linus@oleander.nu>
2011-10-18 12:16:18 +0200

    Using beanstalk_client to push data to beanstalkd during testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,3 +12,27 @@ RSpec.configure do |config|
   config.mock_with :rspec
   config.include Capybara::DSL
 end
+
+#
+# @data String Pushes @data to client
+# Example data being pushed.
+# {
+#   event: "event",
+#   next_station: 8998235,
+#   previous_station: 898345,
+#   arrival_time: 1318843870,
+#   alert_message: "oops!",
+#   line_id: 2342
+#   provider_id: 123123,
+#   journey_id: 123123
+# }
+# Take a look at the readme in the API server js project
+# https://github.com/linjekoll/api-server-js/blob/master/README.md
+#
+
+beanstalk = Beanstalk::Pool.new(['localhost:11300'])
+beanstalk.use("linjekoll.socket-server")
+
+def push(data)
+  beanstalk.put(data)
+end
